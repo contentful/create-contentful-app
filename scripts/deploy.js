3314f02e8ab344cc85fd7ed979a4f60806e7b014
Kado Damball <kadodamball@hotmail.com>
2020-08-14 13:52:57 +0200

    chore: Error handling and clean up

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,32 +1,53 @@
-#! /usr/bin/env node
+const fs = require('fs');
+const spawn = require('cross-spawn');
 
-const fs = require('fs')
-const childProcess = require('child_process')
+const PACKAGES = [
+  '@contentful/create-contentful-app',
+  'create-contentful-app',
+  '@contentful/cra-template-create-contentful-app'
+];
+const MODULE_MAIN_PATH = `${__dirname}/../`;
+const ORIGINAL_PACKAGE_JSON = require(`${MODULE_MAIN_PATH}/package.json`);
 
-const PACKAGES = ['@contentful/create-contentful-app', 'create-contentful-app', '@contentful/cra-template-create-contentful-app']
-const ORIGINAL_PACKAGE_JSON = require('../package.json')
+function restoreFiles() {
+  fs.writeFileSync(
+    `${MODULE_MAIN_PATH}/package.json`,
+    JSON.stringify(ORIGINAL_PACKAGE_JSON, null, 2)
+  );
+  spawn.sync('npm', ['i'], { silent: true, cwd: MODULE_MAIN_PATH });
+}
 
 if (!process.env.NPM_TOKEN) {
-  throw new Error('Missing NPM_TOKEN!')
+  throw new Error('Missing NPM_TOKEN!');
 }
 
-for (const package of PACKAGES) {
-  console.log('')
-  console.log('📦 Deploying package:', package)
-  const packageJson = {...ORIGINAL_PACKAGE_JSON, name: package}
+try {
+  for (const package of PACKAGES) {
+    console.log('');
+    console.log('📦 Deploying package:', package);
+    const packageJson = { ...ORIGINAL_PACKAGE_JSON, name: package };
 
-  console.log(` > 📝 Updating package.json with name: ${package}...`)
-  fs.writeFileSync('../package.json', JSON.stringify(packageJson))
+    console.log(` > 📝 Updating package.json with name: ${package}...`);
+    fs.writeFileSync(`${MODULE_MAIN_PATH}/package.json`, JSON.stringify(packageJson));
 
-  console.log(` > ⚙️  Updating package-lock.json by means of "npm i"...`)
-  childProcess.execSync('npm i', {cwd: '../'})
+    console.log(` > ⚙️  Updating package-lock.json by means of "npm i"...`);
+    spawn.sync('npm', ['i'], { silent: true, cwd: MODULE_MAIN_PATH });
 
-  console.log(` > 📚 Publishing on the registry...`)
-  childProcess.execSync('npm publish', {cwd: '../'})
+    console.log(` > 📚 Publishing ${package} on the registry...`);
+    let { error } = spawn.sync('npm', ['publish', '--access', 'public'], {
+      stdio: 'inherit',
+      cwd: MODULE_MAIN_PATH
+    });
+    if (error) {
+      throw new Error(error);
+    }
 
-  console.log(`✅ Successfully published ${package}@${ORIGINAL_PACKAGE_JSON.version}!`)
-  console.log('')
+    console.log(`✅ Successfully published ${package}@${ORIGINAL_PACKAGE_JSON.version}!`);
+    console.log();
+  }
+} catch (err) {
+  restoreFiles();
+  throw new Error(err);
 }
 
-// Restore original package.json
-fs.writeFileSync('../package.json', JSON.stringify(ORIGINAL_PACKAGE_JSON, null, 2))
+restoreFiles();
