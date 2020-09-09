2b028090ebde5ffc927b06db81a8a14a62b80646
John Whiles <john.whiles@contentful.com>
2020-09-09 11:37:37 +0200

    fix: type errors resulting from updated sdk

--- template/src/index.tsx
+++ template/src/index.tsx
@@ -30,21 +30,30 @@ init((sdk: BaseExtensionSDK) => {
   // Feel free to remove unused locations
   // Dont forget to delete the file too :)
   const ComponentLocationSettings = [
-    { location: locations.LOCATION_APP_CONFIG, component: <Config sdk={sdk as AppExtensionSDK} /> },
+    {
+      location: locations.LOCATION_APP_CONFIG,
+      component: <Config sdk={(sdk as unknown) as AppExtensionSDK} />
+    },
     {
       location: locations.LOCATION_ENTRY_FIELD,
-      component: <Field sdk={sdk as FieldExtensionSDK} />
+      component: <Field sdk={(sdk as unknown) as FieldExtensionSDK} />
     },
     {
       location: locations.LOCATION_ENTRY_EDITOR,
-      component: <EntryEditor sdk={sdk as EditorExtensionSDK} />
+      component: <EntryEditor sdk={(sdk as unknown) as EditorExtensionSDK} />
     },
-    { location: locations.LOCATION_DIALOG, component: <Dialog sdk={sdk as DialogExtensionSDK} /> },
+    {
+      location: locations.LOCATION_DIALOG,
+      component: <Dialog sdk={(sdk as unknown) as DialogExtensionSDK} />
+    },
     {
       location: locations.LOCATION_ENTRY_SIDEBAR,
-      component: <Sidebar sdk={sdk as SidebarExtensionSDK} />
+      component: <Sidebar sdk={(sdk as unknown) as SidebarExtensionSDK} />
     },
-    { location: locations.LOCATION_PAGE, component: <Page sdk={sdk as PageExtensionSDK} /> }
+    {
+      location: locations.LOCATION_PAGE,
+      component: <Page sdk={(sdk as unknown) as PageExtensionSDK} />
+    }
   ];
 
   // Select a component depending on a location in which the app is rendered.
