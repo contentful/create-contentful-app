b5eff1c644752f1fea402c2153bd8848e0fc467b
John Whiles <john.whiles@contentful.com>
2020-10-29 13:27:11 +0100

    chore: fix lint errors in configscreen.spec

--- template/src/components/ConfigScreen.spec.tsx
+++ template/src/components/ConfigScreen.spec.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ConfigScreen from './ConfigScreen';
 import { render } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 
 describe('Config Screen component', () => {
   it('Component text exists', async () => {
@@ -16,7 +15,7 @@ describe('Config Screen component', () => {
     const { getByText } = render(<ConfigScreen sdk={mockSdk} />);
 
     // simulate the user clicking the install button
-    const configurationData = await mockSdk.app.onConfigure.mock.calls[0][0]();
+    await mockSdk.app.onConfigure.mock.calls[0][0]();
 
     expect(
       getByText('Welcome to your contentful app. This is your config page.')
