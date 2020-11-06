a94e8705c80ead8e2a01b486ace2847ef270e70b
John Whiles <john.whiles@contentful.com>
2020-11-06 13:58:48 +0100

    chore: remove config comment

--- template/src/index.tsx
+++ template/src/index.tsx
@@ -64,10 +64,6 @@ if (process.env.NODE_ENV === 'development' && window.self === window.top) {
     ];
 
     // Select a component depending on a location in which the app is rendered.
-    //
-    // NB: Location "app-config" is auto-included in the list as most apps need it
-    // You can remove it (and on the app definition also) in case the app
-    // doesn't require it
     ComponentLocationSettings.forEach(componentLocationSetting => {
       if (sdk.location.is(componentLocationSetting.location)) {
         render(componentLocationSetting.component, root);
