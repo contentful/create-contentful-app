68e4b877ca3583393f5e82768c2f4d4db5c76a9a
Lorenz Weiß <lorenz.weis@gmail.com>
2021-04-12 09:35:41 +0200

    refactor: use adm zip instead of js zip

--- packages/@contentful--app-scripts/lib/upload/create-zip-from-directory.js
+++ packages/@contentful--app-scripts/lib/upload/create-zip-from-directory.js
@@ -1,51 +1,17 @@
-const glob = require('glob');
 const chalk = require('chalk');
-const fs = require('fs').promises;
-const JSZip = require('jszip');
-const util = require('util');
+const AdmZip = require('adm-zip');
 const { showCreationError } = require('../utils');
-
-async function convertPathToFileData(filePath) {
-  return new Promise((res) => {
-    fs.readFile(filePath, 'utf8', (err, data) => {
-      res([filePath, data]);
-    });
-  });
-}
-
-function addFilesToZip(files, zip, path) {
-  files.forEach(([filePath, data]) => {
-    // remove the root path to not have the folder inside the zip
-    let localPath = filePath;
-    const firstIndex = filePath.indexOf(path);
-    if (firstIndex === 0) {
-      localPath = localPath.substring(localPath.length, path.length);
-    }
-    zip.file(localPath, data);
-  });
-  return zip;
-}
-
-const globPromise = util.promisify(glob);
 
 async function createZipFileFromDirectory(path) {
   try {
-    let zip = new JSZip();
-    const filePaths = await globPromise(path + '/**/*', { nodir: true });
-    console.log(`
-${chalk.blueBright('Info:')} Creating a zip file from ${filePaths.length} file${
-      filePaths.length !== 1 ? 's' : ''
-    }.`);
-
-    const files = await Promise.all(filePaths.map(convertPathToFileData));
-    zip = addFilesToZip(files, zip, path);
-    const zipFile = await zip.generateAsync({ type: 'nodebuffer' });
+    const zip = new AdmZip();
+    zip.addLocalFolder(path);
 
     console.log('----------------------------');
     console.log(`${chalk.yellow('Done!')} Files from ${chalk.dim(path)} successfully zipped`);
     console.log('----------------------------');
 
-    return zipFile;
+    return zip.toBuffer();
   } catch (err) {
     showCreationError('zip file', err.message);
     return null;
@@ -53,7 +19,5 @@ ${chalk.blueBright('Info:')} Creating a zip file from ${filePaths.length} file${
 }
 
 module.exports = {
-  convertPathToFileData,
   createZipFileFromDirectory,
-  addFilesToZip,
 };
