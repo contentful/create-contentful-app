86abc98445b9353b05af4f7f7ca8ab33f6f72f2d
Kado Damball <kadodamball@hotmail.com>
2021-04-15 12:02:54 +0200

    Use ignore package and improve readability

--- packages/@contentful--app-scripts/utils/cache-credential/index.test.js
+++ packages/@contentful--app-scripts/utils/cache-credential/index.test.js
@@ -1,20 +1,17 @@
 const { cacheEnvVars } = require('./index');
+const { DOTENV_FILE } = require('../constants');
 const fs = require('fs');
 const assert = require('assert');
 
 const fsPromises = fs.promises;
+const TEMP_DOTENV_FILE = '.env.temp';
 
-describe('Caching environemnt variables', () => {
+describe('Caching environment variables', () => {
   before(async () => {
     try {
-      await fsPromises.access('.env', fs.constants.F_OK);
-      await fsPromises.copyFile('.env', '.env.temp');
-      await fsPromises.unlink('.env');
-      console.log(`
-      
-        RUNNING BEFORE
-      
-      `)
+      await fsPromises.access(DOTENV_FILE, fs.constants.F_OK);
+      await fsPromises.copyFile(DOTENV_FILE, TEMP_DOTENV_FILE);
+      await fsPromises.unlink(DOTENV_FILE);
     } catch(_) {
       // ignore if file doesnt exist
     }
@@ -22,15 +19,10 @@ describe('Caching environemnt variables', () => {
 
   after(async () => {
     try {
-      await fsPromises.unlink('.env')
-      await fsPromises.access('.env.temp', fs.constants.F_OK);
-      await fsPromises.copyFile('.env.temp', '.env');
-      await fsPromises.unlink('.env.temp');
-      console.log(`
-      
-        RUNNING AFTER
-      
-      `)
+      await fsPromises.unlink(DOTENV_FILE)
+      await fsPromises.access(TEMP_DOTENV_FILE, fs.constants.F_OK);
+      await fsPromises.copyFile(TEMP_DOTENV_FILE, DOTENV_FILE);
+      await fsPromises.unlink(TEMP_DOTENV_FILE);
     } catch(_) {
       // ignore if file doesnt exist
     }
@@ -38,20 +30,20 @@ describe('Caching environemnt variables', () => {
 
   it('should create .env file if its missing', async () => {
     await cacheEnvVars({'CONTENTFUL_TOKEN': 'test_value'});
-    const fileExists = (await fsPromises.access('.env', fs.constants.F_OK)) === undefined;
+    const fileExists = (await fsPromises.access(DOTENV_FILE, fs.constants.F_OK)) === undefined;
     assert.ok(fileExists);
   })
 
   it('should replace old variable in .env file if it exists', async () => {
     const envData = `CONTENTFUL_APP_DEF_ID=some_app_def_id\n
 CONTENTFUL_TOKEN=old_value`;
-    const expectedData = `CONTENTFUL_APP_DEF_ID=some_app_def_id\n
+    const expectedData = `CONTENTFUL_APP_DEF_ID=some_app_def_id
 CONTENTFUL_TOKEN=new_value`;
 
-    await fsPromises.writeFile('.env', envData, {encoding: 'utf-8'});
+    await fsPromises.writeFile(DOTENV_FILE, envData, {encoding: 'utf-8'});
 
     await cacheEnvVars({'CONTENTFUL_TOKEN': 'new_value'});
-    const fileData = await fsPromises.readFile('.env', {encoding: 'utf-8'});
+    const fileData = await fsPromises.readFile(DOTENV_FILE, {encoding: 'utf-8'});
     assert.strictEqual(fileData, expectedData);
   })
 
@@ -60,10 +52,10 @@ CONTENTFUL_TOKEN=new_value`;
     const expectedData = `CONTENTFUL_APP_DEF_ID=some_app_def_id
 CONTENTFUL_TOKEN=new_value_2`;
 
-    await fsPromises.writeFile('.env', envData, {encoding: 'utf-8'});
+    await fsPromises.writeFile(DOTENV_FILE, envData, {encoding: 'utf-8'});
 
     await cacheEnvVars({'CONTENTFUL_TOKEN': 'new_value_2'});
-    const fileData = await fsPromises.readFile('.env', {encoding: 'utf-8'});
+    const fileData = await fsPromises.readFile(DOTENV_FILE, {encoding: 'utf-8'});
     assert.strictEqual(fileData, expectedData);
   })
 });
