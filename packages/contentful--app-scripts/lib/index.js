b3c2acc3c4b083e879bf9e57d251cce3c386eef8
Lorenz Weiß <lorenz.weis@gmail.com>
2021-05-04 16:36:12 +0200

    feat: adds simple open command

--- packages/contentful--app-scripts/lib/index.js
+++ packages/contentful--app-scripts/lib/index.js
@@ -1,9 +1,11 @@
 const createAppDefinition = require('./create-app-definition');
 const upload = require('./upload');
 const activate = require('./activate');
+const open = require('./open');
 
 module.exports = {
   createAppDefinition,
   upload,
   activate,
+  open,
 };
