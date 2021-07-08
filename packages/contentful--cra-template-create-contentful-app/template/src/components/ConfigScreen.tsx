1961d742574227dffc6e1bba40457efd3d20b76a
Yuri Mazursky <yuri.mazursky@contentful.com>
2021-07-08 16:53:14 +0200

    feat: use react function component instead of class

--- packages/contentful--cra-template-create-contentful-app/template/src/components/ConfigScreen.tsx
+++ packages/contentful--cra-template-create-contentful-app/template/src/components/ConfigScreen.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useCallback, useState, useEffect } from 'react';
 import { AppExtensionSDK } from '@contentful/app-sdk';
 import { Heading, Form, Workbench, Paragraph } from '@contentful/forma-36-react-components';
 import { css } from 'emotion';
@@ -9,59 +9,58 @@ interface ConfigProps {
   sdk: AppExtensionSDK;
 }
 
-interface ConfigState {
-  parameters: AppInstallationParameters;
-}
+const Config = (props: ConfigProps) => {
+  const [parameters, setParameters] = useState<AppInstallationParameters>({});
 
-export default class Config extends Component<ConfigProps, ConfigState> {
-  constructor(props: ConfigProps) {
-    super(props);
-    this.state = { parameters: {} };
-
-    // `onConfigure` allows to configure a callback to be
-    // invoked when a user attempts to install the app or update
-    // its configuration.
-    props.sdk.app.onConfigure(() => this.onConfigure());
-  }
-
-  async componentDidMount() {
-    // Get current parameters of the app.
-    // If the app is not installed yet, `parameters` will be `null`.
-    const parameters: AppInstallationParameters | null = await this.props.sdk.app.getParameters();
-
-    this.setState(parameters ? { parameters } : this.state, () => {
-      // Once preparation has finished, call `setReady` to hide
-      // the loading screen and present the app to a user.
-      this.props.sdk.app.setReady();
-    });
-  }
-
-  onConfigure = async () => {
+  const onConfigure = useCallback(async () => {
     // This method will be called when a user clicks on "Install"
     // or "Save" in the configuration screen.
     // for more details see https://www.contentful.com/developers/docs/extensibility/ui-extensions/sdk-reference/#register-an-app-configuration-hook
 
     // Get current the state of EditorInterface and other entities
     // related to this app installation
-    const currentState = await this.props.sdk.app.getCurrentState();
+    const currentState = await props.sdk.app.getCurrentState();
 
     return {
       // Parameters to be persisted as the app configuration.
-      parameters: this.state.parameters,
+      parameters,
       // In case you don't want to submit any update to app
       // locations, you can just pass the currentState as is
       targetState: currentState,
     };
-  };
+  }, [parameters, props.sdk]);
 
-  render() {
-    return (
-      <Workbench className={css({ margin: '80px' })}>
-        <Form>
-          <Heading>App Config</Heading>
-          <Paragraph>Welcome to your contentful app. This is your config page.</Paragraph>
-        </Form>
-      </Workbench>
-    );
-  }
+  useEffect(() => {
+    // `onConfigure` allows to configure a callback to be
+    // invoked when a user attempts to install the app or update
+    // its configuration.
+    props.sdk.app.onConfigure(() => onConfigure());
+  }, [props.sdk, onConfigure]);
+
+  useEffect(() => {
+    (async () => {
+      // Get current parameters of the app.
+      // If the app is not installed yet, `parameters` will be `null`.
+      const currentParameters: AppInstallationParameters | null = await props.sdk.app.getParameters();
+
+      if (currentParameters) {
+        setParameters(currentParameters);
+      }
+
+      // Once preparation has finished, call `setReady` to hide
+      // the loading screen and present the app to a user.
+      props.sdk.app.setReady();
+    })()
+  }, [props.sdk])
+
+  return (
+    <Workbench className={css({ margin: '80px' })}>
+      <Form>
+        <Heading>App Config</Heading>
+        <Paragraph>Welcome to your contentful app. This is your config page.</Paragraph>
+      </Form>
+    </Workbench>
+  );
 }
+
+export default Config;
