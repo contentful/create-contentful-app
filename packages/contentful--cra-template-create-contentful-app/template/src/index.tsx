8c73ecbe1b63c1854da7dc66876d0f6eb6df8598
Andi Pätzold <andreas.paetzold@contentful.com>
2021-10-07 09:32:43 +0200

    fix: move createClient into init callback

--- packages/contentful--cra-template-create-contentful-app/template/src/index.tsx
+++ packages/contentful--cra-template-create-contentful-app/template/src/index.tsx
@@ -30,24 +30,24 @@ if (process.env.NODE_ENV === 'development' && window.self === window.top) {
   // You can remove this if block before deploying your app
   const root = document.getElementById('root');
 
-  // Creating a CMA client allows you to use the contentful-management library
-  // within your app. See the contentful-management documentation at https://contentful.github.io/contentful-management.js/contentful-management/latest/
-  // to learn what is possible.
-  const cma = createClient(
-    { apiAdapter: sdk.cmaAdapter },
-    {
-      type: 'plain',
-      defaults: {
-        environmentId: sdk.ids.environment,
-        spaceId: sdk.ids.space,
-      },
-    }
-  );
-
   render(<LocalhostWarning />, root);
 } else {
   init((sdk) => {
     const root = document.getElementById('root');
+
+    // Creating a CMA client allows you to use the contentful-management library
+    // within your app. See the contentful-management documentation at https://contentful.github.io/contentful-management.js/contentful-management/latest/
+    // to learn what is possible.
+    const cma = createClient(
+      { apiAdapter: sdk.cmaAdapter },
+      {
+        type: 'plain',
+        defaults: {
+          environmentId: sdk.ids.environment,
+          spaceId: sdk.ids.space,
+        },
+      }
+    );
 
     // All possible locations for your app
     // Feel free to remove unused locations
