f3f51de3882e52021bf07fe1beac0fb1251714d2
Janko Marklein <janko.marklein@contentful.com>
2021-12-21 12:12:50 +0100

    fix(test): mock missing properties instead of ignoring typecheck

--- packages/contentful--cra-template-create-contentful-app/template/src/components/Field.spec.tsx
+++ packages/contentful--cra-template-create-contentful-app/template/src/components/Field.spec.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import Field from './Field';
 import { render } from '@testing-library/react';
+import { mockCma, mockSdk } from '../../test/mocks';
 
 describe('Field component', () => {
   it('Component text exists', () => {
-    // @ts-ignore This field component should ideally have an sdk and
-    // cma property. 
-    const { getByText } = render(<Field />);
+    const { getByText } = render(<Field cma={mockCma} sdk={mockSdk} />);
 
     expect(getByText('Hello Entry Field Component')).toBeInTheDocument();
   });
