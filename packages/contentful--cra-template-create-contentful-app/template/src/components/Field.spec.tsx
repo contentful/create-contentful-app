b5aba2bd2051610330e4da60ab11eee9642bf95a
Janko Marklein <janko.marklein@contentful.com>
2021-12-21 10:14:19 +0100

    fix(tests): ignore typecheck for test which breaks dev-build

--- packages/contentful--cra-template-create-contentful-app/template/src/components/Field.spec.tsx
+++ packages/contentful--cra-template-create-contentful-app/template/src/components/Field.spec.tsx
@@ -4,6 +4,8 @@ import { render } from '@testing-library/react';
 
 describe('Field component', () => {
   it('Component text exists', () => {
+    // @ts-ignore This field component should ideally have an sdk and
+    // cma property. 
     const { getByText } = render(<Field />);
 
     expect(getByText('Hello Entry Field Component')).toBeInTheDocument();
