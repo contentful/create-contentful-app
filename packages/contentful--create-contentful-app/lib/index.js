7cce1980557a2cb5af76bd6cab84d4d25fc5222c
Andi Pätzold <andreas.paetzold@contentful.com>
2022-02-14 17:27:22 +0100

    feat: remove `init` and `create-definition` command

--- packages/contentful--create-contentful-app/lib/index.js
+++ packages/contentful--create-contentful-app/lib/index.js
@@ -2,7 +2,6 @@
 
 /* eslint-disable no-console, no-process-exit */
 
-import { createAppDefinition } from '@contentful/app-scripts';
 import chalk from 'chalk';
 import degit from 'degit';
 import { readFileSync, writeFileSync } from 'fs';
@@ -14,9 +13,6 @@ import validateAppName from 'validate-npm-package-name';
 import { program } from 'commander';
 import inquirer from 'inquirer';
 
-const localCommand = '@contentful/create-contentful-app';
-const mainCommand = `npx ${localCommand}`;
-
 function successMessage(folder) {
   console.log(`
 ${chalk.cyan('Success!')} Created a new Contentful app in ${chalk.bold(tildify(folder))}.
@@ -24,7 +20,7 @@ ${chalk.cyan('Success!')} Created a new Contentful app in ${chalk.bold(tildify(f
 We suggest that you begin by running:
 
     ${chalk.cyan(`cd ${folder}`)}
-    ${chalk.cyan(`${mainCommand} create-definition`)}
+    ${chalk.cyan(`npm start`)}
   `);
 }
 
@@ -61,6 +57,27 @@ async function promptAppName() {
 
 async function initProject(appName, options) {
   try {
+    if (appName === 'create-definition') {
+      throw new Error(
+        `The ${chalk.bold('create-definition')} command has been removed from ${chalk.cyan(
+          'create-contentful-app'
+        )}.`
+      );
+    }
+
+    if (appName === 'init') {
+      console.log(
+        `${chalk.yellow('Warning:')} The ${chalk.bold(
+          'init'
+        )} command has been removed from ${chalk.cyan(
+          'create-contentful-app'
+        )}. You can now create new apps running ${chalk.cyan(
+          'npx create-contentful-app'
+        )} directly.`
+      );
+      appName = undefined;
+    }
+
     if (!appName) {
       const prompt = await promptAppName();
       appName = prompt.name;
@@ -119,28 +136,15 @@ async function initProject(appName, options) {
 
 (async function main() {
   program
-    .command('init', { isDefault: true })
-    .description('Bootstrap your app inside a new folder ‘app-name’')
-    .argument('[app-name]', 'App name')
-    .option('--npm', 'Use NPM')
-    .option('--yarn', 'Use Yarn')
-    .option('--javascript, -js')
-    .option('--typescript, -ts')
+    .name(`npx ${chalk.cyan('@contentful/create-contentful-app')}`)
+    .helpOption(true)
+    .description('bootstraps your app into a newly created directory')
+    .argument('[app-name]', 'app name')
+    .option('--npm', 'use NPM')
+    .option('--yarn', 'use Yarn')
+    .option('--javascript, -js', 'use JavaScript')
+    .option('--typescript, -ts', 'use TypeScript')
     .action(initProject);
-
-  program
-    .command('create-definition')
-    .description(
-      'Creates an app definition for your app in a Contentful organization of your choice.'
-    )
-    .action(async () => {
-      await createAppDefinition.interactive();
-    });
-
-  program
-    .name(chalk.cyan('@contentful/create-contentful-app'))
-    .usage(chalk.cyan('[options] {[app-name]|[command]}'))
-    .helpOption(false);
 
   await program.parseAsync();
 })();
