06775107ff7c60644a93de726571b71bf7a2718d
Martin Walker <martin.walker@contentful.com>
2022-03-04 11:19:27 +0100

    rename useCMA hook, depend on contentful-management ^7.13.0

---
+++ packages/contentful--react-apps-toolkit/src/useCMA.spec.ts
@@ -0,0 +1,37 @@
+import { renderHook } from '@testing-library/react-hooks';
+import { useCMA } from './useCMA';
+
+const mockedCma: string | undefined = "mocked-cma";
+
+const mockedSdk = {
+  cmaAdapter: "placholder",
+  ids: {
+    environment: "placeholder",
+    space: "placeholder"
+  }
+}
+
+jest.mock('./useSDK', () => ({
+  ...jest.requireActual('./useSDK'),
+  useSDK: () => mockedSdk
+}));
+
+jest.mock('./SDKProvider', () => ({
+  SDKContext: {},
+}));
+
+jest.mock('contentful-management', () => {
+  return {
+    ...jest.requireActual('contentful-management'),
+    createClient: () => mockedCma
+  }
+})
+
+describe('useCMA', () => {
+  test('should return cma client', () => {
+
+    const { result } = renderHook(() => useCMA());
+
+    expect(result.current).toBe(mockedCma);
+  })
+})
