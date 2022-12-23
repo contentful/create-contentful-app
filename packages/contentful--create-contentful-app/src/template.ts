191e9c506bed710f7c2f9bdc454097d923ae1a30
Andi Pätzold <andreas.paetzold@contentful.com>
2022-12-23 17:45:02 +0100

    fix: tweak example vs blank question

--- packages/contentful--create-contentful-app/src/template.ts
+++ packages/contentful--create-contentful-app/src/template.ts
@@ -34,7 +34,7 @@ async function promptExampleSelection(): Promise<string> {
   const { starter } = await inquirer.prompt([
     {
       name: 'starter',
-      message: 'Do you want to start with an example template or use one of the blank templates?',
+      message: 'Do you want to start with an example or use one of our blank templates?',
       type: 'list',
       choices: ['blank', 'example'],
       default: 'blank',
