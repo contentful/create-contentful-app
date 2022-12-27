406f8831c1ca7141405510ca5dabaecec31fa3aa
Yuri Mazursky <colomolo@users.noreply.github.com>
2022-12-27 20:41:27 +0500

    feat: [EXT-4148] Suggest to create an app definition (#1344)
    * feat: suggest to create an app definition
    
    * feat: tweak app definition creation copy
    
    * chore: tweak copy
    
    * chore: use shortened link for tutorial
    
    * chore: tweak start copy

--- packages/contentful--create-contentful-app/src/index.ts
+++ packages/contentful--create-contentful-app/src/index.ts
@@ -13,6 +13,7 @@ import { detectManager, exec, normalizeOptions } from './utils';
 import { CLIOptions } from './types';
 import { code, error, highlight, success, warn, wrapInBlanks } from './logger';
 import chalk from 'chalk';
+import { CREATE_APP_DEFINITION_GUIDE_URL, EXAMPLES_REPO_URL } from './constants';
 
 const DEFAULT_APP_NAME = 'contentful-app';
 
@@ -22,7 +23,16 @@ ${success('Success!')} Created a new Contentful app in ${highlight(tildify(folde
 
   wrapInBlanks(highlight('---- Next Steps'))
 
-  console.log(`Now kick it off by running
+  console.log(`Now create an app definition for your app by running
+
+    ${code(`cd ${tildify(folder)}`)}
+    ${code(useYarn ? 'yarn create-app-definition' : 'npm run create-app-definition')}
+
+    or you can create it manually in web app:
+    ${highlight(CREATE_APP_DEFINITION_GUIDE_URL)}
+  `);
+
+  console.log(`Then kick it off by running
 
     ${code(`cd ${tildify(folder)}`)}
     ${code(`${useYarn ? 'yarn' : 'npm'} start`)}
@@ -96,7 +106,6 @@ async function initProject(appName: string, options: CLIOptions) {
 
     console.log(`Creating a Contentful app in ${highlight(tildify(fullAppFolder))}.`);
 
-
     await cloneTemplateIn(fullAppFolder, normalizedOptions);
 
     updatePackageName(fullAppFolder);
@@ -131,9 +140,7 @@ async function initProject(appName: string, options: CLIOptions) {
         '',
         code('  create-contentful-app my-app --source "github:user/repo"'),
         '',
-        `Official Contentful templates are hosted at ${highlight(
-          'https://github.com/contentful/apps/tree/master/examples'
-        )}.`,
+        `Official Contentful templates are hosted at ${highlight(EXAMPLES_REPO_URL)}.`,
       ].join('\n')
     )
     .argument('[app-name]', 'app name')
@@ -143,7 +150,7 @@ async function initProject(appName: string, options: CLIOptions) {
     .option('-js, --javascript', 'use JavaScript template')
     .option(
       '-e, --example <example-name>',
-      'bootstrap an example app from https://github.com/contentful/apps/tree/master/examples'
+      `bootstrap an example app from ${EXAMPLES_REPO_URL}`
     )
     .option(
       '-s, --source <url>',
