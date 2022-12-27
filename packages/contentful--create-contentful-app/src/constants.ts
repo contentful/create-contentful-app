17bdfb1c0f91414e6fb7fa704b8a03f3d62a1870
Yuri Mazursky <yuri.mazursky@contentful.com>
2022-12-27 16:11:08 +0100

    chore: use shortened link for tutorial

--- packages/contentful--create-contentful-app/src/constants.ts
+++ packages/contentful--create-contentful-app/src/constants.ts
@@ -1,3 +1,3 @@
 export const CREATE_APP_DEFINITION_GUIDE_URL =
-  'https://www.contentful.com/developers/docs/extensibility/app-framework/tutorial/#embed-your-app-in-the-contentful-web-app';
+  'https://ctfl.io/app-tutorial#embed-your-app-in-the-contentful-web-app';
 export const EXAMPLES_REPO_URL = 'https://github.com/contentful/apps/tree/master/examples';
