7e71506ed6e1307c5884aedfae177988036d033c
pkeavenycontentful <pkeaveny@contentful.com>
2023-04-26 16:48:21 -0600

    Update useAutoResizer.ts

--- packages/contentful--vue-apps-toolkit/src/useAutoResizer.ts
+++ packages/contentful--vue-apps-toolkit/src/useAutoResizer.ts
@@ -1,10 +1,16 @@
-import vue from 'vue'
+import { onMounted, onUnmounted } from 'vue';
 import {
     DialogExtensionSDK,
     FieldExtensionSDK,
     SidebarExtensionSDK,
     WindowAPI,
+    KnownAppSDK,
+    init as initContentfulApp,
+    SidebarAppSDK,
+    FieldAppSDK,
+    DialogAppSDK
 } from '@contentful/app-sdk';
+
 
 /*
 https://learnvue.co/articles/vue-3-plugins-provide-and-inject
@@ -20,6 +26,22 @@ const app = createApp('#root');
 app.provide(useAutoResizer)
 */
 
-export function useAutoResizer() {
-    console.log('yes')
+export function useAutoResizer(...params: Parameters<WindowAPI['startAutoResizer']>) {
+
+    initContentfulApp((sdk: SidebarAppSDK | FieldAppSDK | DialogAppSDK) => {
+
+        onMounted(() => {
+            if (typeof sdk === 'undefined') {
+                throw new Error(
+                    'useAutoResizer is unavailable at this location, it can only be rendered in field, sidebar and dialog locations.'
+                );
+            }
+
+            sdk.window.startAutoResizer(...params);
+        });
+
+        onUnmounted(() => {
+            sdk.window.stopAutoResizer();
+        });
+    });
 }
