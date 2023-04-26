4799d1254f819c5a846d16bda5ffca853225c930
pkeavenycontentful <pkeaveny@contentful.com>
2023-04-26 16:07:16 -0600

    Added useSDK for reactive variables

--- packages/contentful--vue-apps-toolkit/src/useSDK.ts
+++ packages/contentful--vue-apps-toolkit/src/useSDK.ts
@@ -1,6 +1,17 @@
-import vue from 'vue'
+import { reactive } from 'vue'
+import { init } from '@contentful/app-sdk'
+import { KnownAppSDK } from '@contentful/app-sdk';
 
 
-export function useSDK() {
+export async function useSDK(): Promise<KnownAppSDK> {
+    let data = reactive({ sdk: {} });
 
+    const SDKData = new Promise(async (resolve) => {
+        return init((sdk: KnownAppSDK) => {
+            data.sdk = sdk;
+            resolve(sdk);
+        });
+    });
+    await SDKData;
+    return <KnownAppSDK>data.sdk;
 }
