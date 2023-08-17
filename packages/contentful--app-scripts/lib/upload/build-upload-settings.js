e7fd81dd7535dc79c0ec1341f78a511e6af8bf76
Yuri Mazursky <colomolo@users.noreply.github.com>
2023-08-17 12:30:30 +0200

    feat: [] Prompt all existing options in interactive mode (#1631)
    * feat: prompt for optional params in interactive mode
    
    * docs: clean up
    
    * feat: tweak wording and params handling
    
    * fix: bundle activation input

--- packages/contentful--app-scripts/lib/upload/build-upload-settings.js
+++ packages/contentful--app-scripts/lib/upload/build-upload-settings.js
@@ -3,6 +3,7 @@
 const inquirer = require('inquirer');
 const { getAppInfo } = require('../get-app-info');
 const { getActionsManifest } = require('../utils');
+const { CONTENTFUL_API_HOST } = require('../../utils/constants');
 
 async function buildAppUploadSettings(options) {
   const actionsManifest = getActionsManifest();
@@ -22,14 +23,29 @@ async function buildAppUploadSettings(options) {
       default: '',
     });
   }
+  if (skipActivation === undefined) {
+    prompts.push({
+      type: 'confirm',
+      name: 'activateBundle',
+      message: `Do you want to activate the bundle after upload?`,
+      default: true,
+    });
+  }
+  if (!host) {
+    prompts.push({
+      name: 'host',
+      message: `Contentful CMA endpoint URL:`,
+      default: CONTENTFUL_API_HOST,
+    });
+  }
 
-  const appUploadSettings = await inquirer.prompt(prompts);
+  const { activateBundle, ...appUploadSettings } = await inquirer.prompt(prompts);
 
   const appInfo = await getAppInfo(options);
   // Add app-config & dialog automatically
   return {
     bundleDirectory: bundleDir,
-    skipActivation: !!skipActivation,
+    skipActivation: skipActivation === undefined ? !activateBundle : skipActivation,
     comment,
     host,
     actions: actionsManifest,
