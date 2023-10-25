88a073b117de8061e0646eb3dc9a9cb8f4507f10
Piotr Ciążyński <piotr.ciazynski@contentful.com>
2023-10-25 11:44:44 +0200

    chore: update the templates list [MONET-1564] (#1746)
    * chore: update the templates list [MONET-1564]
    
    * chore: rename TEMPLATES to IGNORED_EXAMPLE_FOLDERS

--- packages/contentful--create-contentful-app/src/constants.ts
+++ packages/contentful--create-contentful-app/src/constants.ts
@@ -1,10 +1,15 @@
 export const CREATE_APP_DEFINITION_GUIDE_URL =
   'https://ctfl.io/app-tutorial#embed-your-app-in-the-contentful-web-app';
 export const EXAMPLES_REPO_URL = 'https://github.com/contentful/apps/tree/master/examples';
-export const TEMPLATES = ['javascript', 'typescript', 'vue', 'vite-react', 'nextjs'] as const;
+export const IGNORED_EXAMPLE_FOLDERS = [
+  'javascript',
+  'typescript',
+  'vue',
+  'vite-react',
+  'nextjs',
+  'hosted-app-action-templates',
+  'hosted-delivery-function-templates',
+] as const;
 export const EXAMPLES_PATH = 'contentful/apps/examples/';
 export const CONTENTFUL_APP_MANIFEST = 'contentful-app-manifest.json';
-export const IGNORED_CLONED_FILES = [
-  CONTENTFUL_APP_MANIFEST,
-  `package.json`
-]
+export const IGNORED_CLONED_FILES = [CONTENTFUL_APP_MANIFEST, `package.json`];
