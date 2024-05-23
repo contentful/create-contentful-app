1ab81194df63aed72e823fcf824cdf3b1597a4ad
Jared Jolton <jared.jolton@contentful.com>
2024-05-23 15:26:03 -0600

    feat: add nudge about app event function workflow to upload flow [EXT-5160]

--- packages/contentful--app-scripts/src/upload/index.ts
+++ packages/contentful--app-scripts/src/upload/index.ts
@@ -5,8 +5,20 @@ import { buildAppUploadSettings } from './build-upload-settings';
 import { UploadOptions, UploadSettings } from '../types';
 import { logFeedbackNudge } from '../feedback/feedback';
 
+function uploadSettingsHaveAppEventFunction(settings: UploadSettings) {
+  return (
+    settings.functions &&
+    settings.functions.some((fn) => fn.accepts.some((fnType) => fnType.startsWith('appevent')))
+  );
+}
+
 async function uploadAndActivate(settings: UploadSettings) {
   const bundle = await createAppBundleFromSettings(settings);
+  if (uploadSettingsHaveAppEventFunction(settings)) {
+    console.log(
+      'Remember that, in order to be invoked, your App Event function(s) must be linked to your App Event subscription using the CMA or the Events tab of the App Details page.'
+    );
+  }
   if (!settings.skipActivation && bundle) {
     await activateBundle({ ...settings, bundleId: bundle.sys.id });
     logFeedbackNudge();
