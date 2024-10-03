653959bbd95223e7a4ecb26e54c329207837a9aa
Jared Jolton <145477871+jjolton-contentful@users.noreply.github.com>
2024-10-03 14:57:06 -0600

    fix: force update in packages/create-contentful-app [] (#2160)

--- packages/create-contentful-app/index.js
+++ packages/create-contentful-app/index.js
@@ -1,3 +1,7 @@
 #!/usr/bin/env node
 
+// This is a convenience wrapper.
+// It allows you to run `npx create-contentful-app` directly,
+// instead of `npx @contentful/create-contentful-app`.
+
 import '@contentful/create-contentful-app';
