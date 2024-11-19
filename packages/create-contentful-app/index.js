1802a80a9f7c5e6ed3aec57ea10c697a1ca7dfbb
Lisa White <lisa.white@contentful.com>
2024-11-19 11:04:05 -0700

    fix: suppress warnings in CCA [] (#2204)

--- packages/create-contentful-app/index.js
+++ packages/create-contentful-app/index.js
@@ -4,4 +4,9 @@
 // It allows you to run `npx create-contentful-app` directly,
 // instead of `npx @contentful/create-contentful-app`.
 
+// The following warning is emitted from eslint: [DEP0040] DeprecationWarning: The `punycode` module is deprecated. Please use a userland alternative instead.
+// eslint has a dependency on ajv v6, which is where this punycode deprecation warning is coming from
+// this warning is visible to users when running the create-contentful-app command on node v22, so we are suppressing warnings here until dependencies are updated
+process.removeAllListeners('warning');
+
 import '@contentful/create-contentful-app';
