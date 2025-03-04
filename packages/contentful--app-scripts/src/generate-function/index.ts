2a8cb72b2620337eb0b9a6a5a5990df5c2a25807
michaelpineirocontentful <michael.pineiro@contentful.com>
2025-03-04 14:59:51 -0700

    fix: Simplified input and correct navigation in new structure
    chore: fixed tests
    
    fix: testing and linting
    
    fix: fix test
    
    fix: verbiage
    
    chore: Changed 'source' to 'example'

--- packages/contentful--app-scripts/src/generate-function/index.ts
+++ packages/contentful--app-scripts/src/generate-function/index.ts
@@ -1,15 +1,15 @@
-import { GenerateFunctionOptions } from "../types";
-import { buildGenerateFunctionSettings, buildGenerateFunctionSettingsFromOptions } from "./build-generate-function-settings";
+import { GenerateFunctionSettings } from "../types";
+import { buildGenerateFunctionSettingsInteractive, buildGenerateFunctionSettingsCLI } from "./build-generate-function-settings";
 import { create } from "./create-function";
 
 const interactive = async () => {
-  const generateFunctionSettings = await buildGenerateFunctionSettings();
+  const generateFunctionSettings = await buildGenerateFunctionSettingsInteractive();
 
   return create(generateFunctionSettings);
 };
 
-const nonInteractive = async (options: GenerateFunctionOptions) => {
-    const generateFunctionSettings = await buildGenerateFunctionSettingsFromOptions(options);
+const nonInteractive = async (options: GenerateFunctionSettings) => {
+    const generateFunctionSettings = await buildGenerateFunctionSettingsCLI(options);
     return create(generateFunctionSettings);
 };
 
