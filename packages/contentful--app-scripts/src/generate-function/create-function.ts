f869587d24baf019cee2205ff41e9c18525a7e62
Michael Pineiro <michael.pineiro@contentful.com>
2025-03-07 14:34:44 -0700

    fix: no duplicate app manifests and better function name validation generate-function and more thorough testing (#2373)
    feat: custom exception
    
    fix: don't lowercase function names
    
    fix: lint
    
    fix: Better testing, better error propogation
    
    fix: Better message for whether banned names or alphanumeric

--- packages/contentful--app-scripts/src/generate-function/create-function.ts
+++ packages/contentful--app-scripts/src/generate-function/create-function.ts
@@ -5,6 +5,5 @@ import { GenerateFunctionSettings } from "../types";
 export async function create(settings: GenerateFunctionSettings) {
   const localPath = resolve(process.cwd());
   await cloneFunction(localPath, settings);
-
   console.log(`Function "${settings.name}" created successfully`);
 }
