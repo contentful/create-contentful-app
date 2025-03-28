8d0393797a613d0f7ad07a652b097dc01d1cb3ae
michaelpineirocontentful <michael.pineiro@contentful.com>
2025-03-28 13:22:58 -0600

    feat: generate-function accepts keepPackageJson flag
    fix: package.json in correct directory

--- packages/contentful--app-scripts/src/generate-function/constants.ts
+++ packages/contentful--app-scripts/src/generate-function/constants.ts
@@ -1,7 +1,7 @@
 export const EXAMPLES_PATH = 'contentful/apps/function-examples/';
 export const APP_MANIFEST = 'app-manifest.json';
 export const CONTENTFUL_APP_MANIFEST = 'contentful-app-manifest.json';
-export const IGNORED_CLONED_FILES = [APP_MANIFEST, CONTENTFUL_APP_MANIFEST, `package.json`];
+export const IGNORED_CLONED_FILES = [APP_MANIFEST, CONTENTFUL_APP_MANIFEST];
 export const REPO_URL = 'https://github.com/contentful/apps/function-examples';
 
 export const ACCEPTED_EXAMPLE_FOLDERS = [
