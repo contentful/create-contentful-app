dd19a0ee564db3b33df5ce7a3f22627549ae5bf8
michaelpineirocontentful <michael.pineiro@contentful.com>
2025-06-13 10:44:19 -0600

    fix: revert inflight overwrites [EXT-6447]
    Revert "fix: remove all references to inflight (#2530)"
    
    This reverts commit a53b6b6fbab4cbfe31fc30020657026be1a4e9d5.
    
    Revert "fix: rebump all packages [EXT-6447] (#2536)"
    
    This reverts commit c7192fd2a390605b3c320f74e0b3f850e241c4ab.
    
    Revert "fix: update create-contentful-app remove inflight (#2533)"
    
    This reverts commit b8a7dd1827507ad68352fd5bef55cb0c47a55f50.

--- packages/create-contentful-app/index.js
+++ packages/create-contentful-app/index.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-// This is a convenience wrapper!
+// This is a convenience wrapper.
 // It allows you to run `npx create-contentful-app` directly,
 // instead of `npx @contentful/create-contentful-app`.
 
