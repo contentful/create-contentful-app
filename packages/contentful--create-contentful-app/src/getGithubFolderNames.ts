b80774b6950c2dc69f75c957aaf6733aadb6b397
BobHemphill76 <robert.hemphill@contentful.com>
2026-01-07 08:13:11 -0700

    chore: move to GH [EXT-7081] (#2823)
    * chore: move to GH [EXT-7081]
    
    * Fix/ext 7081/revert dependabot (#2858)
    
    * Revert "chore(deps-dev): bump ts-jest from 27.1.3 to 29.4.6 (#2817)"
    
    This reverts commit 2bf266a2d252cf46c756efd8489e09df245c2e8b.
    
    * Revert "fix(deps): bump zod from 3.25.76 to 4.2.1 (#2840)"
    
    This reverts commit 11d5c6b0751a0952c9cffc0adad43fd7612e8c4d.
    
    * Revert "fix(deps): bump inquirer from 8.2.6 to 13.1.0 (#2831)"
    
    This reverts commit c6620397e0f0d3e418b09e3d57d2025c4a7fbf1d.
    
    * Revert "chore(deps): bump qs in /packages/contentful--create-contentful-app (#2844)"
    
    This reverts commit 5e7d5e71e4eb6f3fa8aa2b696c4f73e82fa3a7fb.
    
    * Revert "chore(deps-dev): bump eslint from 8.57.0 to 9.39.2 (#2829)"
    
    This reverts commit 189a74080cb9a63b937184c14bf8b68c203e9e21.
    
    * Revert "fix(deps): bump esbuild from 0.27.0 to 0.27.2 (#2842)"
    
    This reverts commit 5921b8ab53c96fc8718d247be3035d7513d3ac43.
    
    * Revert "chore(deps): bump qs in /packages/create-contentful-app (#2845)"
    
    This reverts commit e5130b8b9b888874efc500d701cc82e2d64172e2.
    
    * Revert "chore(deps-dev): bump @testing-library/react from 12.1.5 to 16.3.1 (#2838)"
    
    This reverts commit 03291e0dcbfdb89a8e2885b730e156144c7af418.
    
    * Revert "chore(deps-dev): bump react-dom from 17.0.2 to 19.2.3 (#2828)"
    
    This reverts commit c45d0fffc5d8eb059af6bd0671f7648e7cd2003a.

--- packages/contentful--create-contentful-app/src/getGithubFolderNames.ts
+++ packages/contentful--create-contentful-app/src/getGithubFolderNames.ts
@@ -1,4 +1,4 @@
-import fetch from 'node-fetch';
+import fetch, { RequestInit } from 'node-fetch';
 import { HTTPResponseError } from './types';
 import chalk from 'chalk';
 
@@ -12,7 +12,14 @@ export const CONTENTFUL_APPS_EXAMPLE_FOLDER =
 
 export async function getGithubFolderNames(): Promise<string[]> {
   try {
-    const response = await fetch(CONTENTFUL_APPS_EXAMPLE_FOLDER);
+    const options: RequestInit = {};
+    const githubToken = process.env.GITHUB_TOKEN;
+    if (githubToken) {
+      options.headers = {
+        Authorization: `Bearer ${githubToken}`,
+      };
+    }
+    const response = await fetch(CONTENTFUL_APPS_EXAMPLE_FOLDER, options);
     if (!response.ok) {
       throw new HTTPResponseError(
         `${chalk.red('Error:')} Failed to fetch Contentful app templates: ${response.status} ${
