7ec7018786ae60fb7ac873c8bc88079aa7a1299e
Bob Hemphill <robert.hemphill@contentful.com>
2026-01-20 10:37:50 -0700

    fix: logging again 3[EXT-7081]

--- packages/contentful--create-contentful-app/src/index.ts
+++ packages/contentful--create-contentful-app/src/index.ts
@@ -45,7 +45,7 @@ ${success('Success!')} Created a new Contentful app in ${highlight(tildify(folde
     ${code(command)}
 
     or you can create it manually in web app:
-    ${highlight(CREATE_APP_DEFINITION_GUIDE_URL)}
+    ${highlight(CREATE_APP_DEFINITION_GUIDE_URL)}.
   `);
 
   console.log(`Then kick it off by running
@@ -122,7 +122,7 @@ async function initProject(appName: string, options: CLIOptions) {
 
     const fullAppFolder = resolve(process.cwd(), appName);
 
-    console.log(`Creating a Contentful app in ${highlight(tildify(fullAppFolder))}.`);
+    console.log(`Creating a Contentful app in ${highlight(tildify(fullAppFolder))}`);
 
     if (normalizedOptions.function && normalizedOptions.skipUi) {
       await addFunctionTemplate(fullAppFolder);
